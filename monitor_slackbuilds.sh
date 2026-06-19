13d105b6b7a6fb847d91235bc043d3ecd5a84ec9
Andrew Clemons <andrew.clemons@gmail.com>
2026-06-20 08:39:22 +0900

    Track gitlab-cli releases.

--- monitor_slackbuilds.sh
+++ monitor_slackbuilds.sh
@@ -113,6 +113,8 @@ fi
     CURRENT="$(w3m_fetch "https://sourceforge.net/projects/emailrelay/files/emailrelay/" | sed -n '/^     Name/,$p' | sed -n '3p' | sed 's/^[[:space:]]*//' | sed 's/\([^ ]*\).*$/\1/')"
   elif [[ $PRGNAM == gajim ]]; then
     CURRENT="$(curl -f -s -H "Accept: application/json" "https://dev.gajim.org/api/v4/projects/30/repository/tags" | jq -r '.[0] | .name')"
+  elif [[ $PRGNAM == gitlab-cli ]] ; then
+    CURRENT="$(curl -f -s -H "Accept: application/json" "https://gitlab.com/api/v4/projects/34675721/repository/tags" | jq -r '.[0] | .name')"
   elif [[ $PRGNAM == gopls ]]; then
     CURRENT="$(w3m_fetch https://pkg.go.dev/golang.org/x/tools/gopls?tab=versions | sed -n '/Versions in this module/,$p' | sed 's/^v0$//' | sed -n '/^v/{s/^v//p;q}')"
   elif [[ $PRGNAM == jenkins ]]; then
