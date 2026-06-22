1e58bd9b66c1c1387e761fbd63ed105a9c5b3f6a
Andrew Clemons <andrew.clemons@gmail.com>
2026-06-22 18:51:11 +0900

    Trim leading `v` on gitlab-cli releases.

--- monitor_slackbuilds.sh
+++ monitor_slackbuilds.sh
@@ -100,7 +100,7 @@ fi
   fi
 
   if [[ $PRGNAM == binfmt-support ]] ; then
-    CURRENT="$(curl -f -s -H "Accept: application/json" "https://gitlab.com/api/v4/projects/22757105/repository/tags" | jq -r '.[0] | .name' | sed 's/^v//')"
+    CURRENT="$(curl -f -s -H "Accept: application/json" "https://gitlab.com/api/v4/projects/22757105/repository/tags" | jq -r '.[0] | .name')"
   elif [[ $PRGNAM == eclipse-cpp ]] || [[ $PRGNAM == eclipse-java ]] || [[ $PRGNAM == eclipse-jee ]] || [[ $PRGNAM == eclipse-php ]] ; then
     CURRENT="$(w3m_fetch "https://www.eclipse.org/downloads/packages/" | sed '/^Eclipse /!d' | sed -n '/^Eclipse IDE.*Packages/p' | sed 's/^Eclipse \(.*\) R Packages.*$/\1/;s/-//g;s/IDE //')"
 
@@ -116,7 +116,7 @@ fi
   elif [[ $PRGNAM == gajim ]]; then
     CURRENT="$(curl -f -s -H "Accept: application/json" "https://dev.gajim.org/api/v4/projects/30/repository/tags" | jq -r '.[0] | .name')"
   elif [[ $PRGNAM == gitlab-cli ]] ; then
-    CURRENT="$(curl -f -s -H "Accept: application/json" "https://gitlab.com/api/v4/projects/34675721/repository/tags" | jq -r '.[0] | .name')"
+    CURRENT="$(curl -f -s -H "Accept: application/json" "https://gitlab.com/api/v4/projects/34675721/repository/tags" | jq -r '.[0] | .name' | sed 's/^v//')"
   elif [[ $PRGNAM == gopls ]]; then
     CURRENT="$(w3m_fetch https://pkg.go.dev/golang.org/x/tools/gopls?tab=versions | sed -n '/Versions in this module/,$p' | sed 's/^v0$//' | sed -n '/^v/{s/^v//p;q}')"
   elif [[ $PRGNAM == jenkins ]]; then
